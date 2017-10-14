2e90b261db6ade48efff3e77834a2922e4ff9c79
DavidLee999 <MoonLee003@outlook.com>
2017-10-14 20:00:03 +0200

    decision tree, including gain_ratio and gini_index

--- Ch3_DecisionTree/trees.py
+++ Ch3_DecisionTree/trees.py
@@ -180,3 +180,18 @@ def createTree(dataSet, labels):
         myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), subLabels)
 
     return myTree
+
+
+def classify(inputTree, featLabels, testVec):
+    firstStr = inputTree.keys()[0]
+    secondDict = inputTree[firstStr]
+
+    featIndex = featLabels.index(firstStr)
+    for key in secondDict.keys():
+        if testVec[featIndex] == key:
+            if type(secondDict[key]).__name__ == 'dict':
+                classLabel = classify(secondDict[key], featLabels, testVec)
+            else:
+                classLabel = secondDict[key]
+
+    return classLabel
