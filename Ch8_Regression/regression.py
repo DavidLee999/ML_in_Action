5c74e7ffb4c9b78de29d319b2ff2931faec8d4cb
PenghuaLI <MoonLee003@outlook.com>
2017-10-29 17:21:54 +0100

    Add files via upload

--- Ch8_Regression/regression.py
+++ Ch8_Regression/regression.py
@@ -126,3 +126,92 @@ def stageWise(xArr, yArr, eps = 0.01, numIt = 100):
         returnMat[i, :] = ws.T
 
     return returnMat
+
+from bs4 import BeautifulSoup
+
+def scrapePage(retX, retY, inFile, yr, numPce, origPrc):
+    fr = open(inFile)
+    soup = BeautifulSoup(fr.read())
+    i = 1
+    
+    currentRow = soup.findAll('table', r = '%d' %i)
+    while (len(currentRow) != 0):
+        currentRpw = soup.findAll('table', r = '%d' %i)
+        title = currentRow[0].findAll('a')[1].text
+        lwrTitle = title.lower()
+        
+        if (lwrTitle.find('new') > -1) or (lwrTitle.find('nisb') > -1):
+            newFlag = 1.0
+        else:
+            newFlag = 0.0
+           
+        soldUnicde = currentRow[0].findAll('td')[3].findAll('span')
+        if len(soldUnicde) == 0:
+            print "item #%d did not sell" %i
+        else:
+            soldPrice = currentRow[0].findAll('td')[4]
+            priceStr = soldPrice.text
+            priceStr = priceStr.replace('$','')
+            priceStr = priceStr.replace(',','')
+            if len(soldPrice) > 1:
+                priceStr = priceStr.replace('Free shipping', '')
+            sellingPrice = float(priceStr)
+            if  sellingPrice > origPrc * 0.5:
+                print "%d\t%d\t%d\t%f\t%f" % (yr,numPce,newFlag,origPrc, sellingPrice)
+                retX.append([yr, numPce, newFlag, origPrc])
+                retY.append(sellingPrice)
+        i += 1
+        
+        currentRow = soup.findAll('table', r="%d" % i)
+
+def setDataCollect(retX, retY):
+    scrapePage(retX, retY, 'setHtml/lego8288.html', 2006, 800, 49.99)
+    scrapePage(retX, retY, 'setHtml/lego10030.html', 2002, 3096, 269.99)
+    scrapePage(retX, retY, 'setHtml/lego10179.html', 2007, 5195, 499.99)
+    scrapePage(retX, retY, 'setHtml/lego10181.html', 2007, 3428, 199.99)
+    scrapePage(retX, retY, 'setHtml/lego10189.html', 2008, 5922, 299.99)
+    scrapePage(retX, retY, 'setHtml/lego10196.html', 2009, 3263, 249.99)
+    
+def crossValidation(xArr, yArr, numVal = 10):
+    m = len(yArr)
+    indexList = range(m)
+    errorMat = zeros((numVal, 30))
+    
+    for i in range(numVal):
+        trainX = []
+        trainY = []
+        testX = []
+        testY = []
+        
+        random.shuffle(indexList)
+        for j in range(m): # 10-folder cross validation
+            if j < m * 0.9:
+                trainX.append(xArr[indexList[j]])
+                trainY.append(yArr[indexList[j]])
+            else:
+                testX.append(xArr[indexList[j]])
+                testY.append(yArr[indexList[j]])
+        
+        wMat = ridgeTest(trainX, trainY)
+        for k in range(30):
+            matTestX = mat(testX)
+            matTrainX = mat(trainX)
+            meanTrain = mean(matTrainX, 0)
+            varTrain = var(matTrainX, 0)
+            
+            matTestX = (matTestX - meanTrain) / varTrain # regularize test data as the training data does
+            yEst = matTestX * mat(wMat[k, :]).T + mean(trainY)
+            errorMat[i, k] = rssError(yEst.T.A, array(testY))
+    
+    meanErrors = mean(errorMat, 0)
+    minMean = float(min(meanErrors))
+    bestWeights = wMat[nonzero(meanErrors == minMean)]
+    
+    xMat = mat(xArr)
+    yMat = mat(yArr).T
+    meanX = mean(xMat, 0)
+    varX = var(xMat, 0)
+    unReg = bestWeights / varX
+    
+    print "the best model from Ridge Regression is \n", unReg
+    print "with constant term: ", -1 * sum(multiply(meanX, unReg)) + mean(yMat)
