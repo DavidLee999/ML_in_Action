45d3c41897a1f5b7fe3c0c40768e25debb414e0a
DavidLee999 <MoonLee003@outlook.com>
2017-10-29 11:33:56 +0100

    linear regression

--- Ch8_Regression/regression.py
+++ Ch8_Regression/regression.py
@@ -58,6 +58,71 @@ def lwlrTest(testArr, xArr, yArr, k = 1.0):
 
     return yHat
 
-def resErr(yArr, yHatArr):
+def rssError(yArr, yHatArr):
     return ((yArr - yHatArr) ** 2).sum()
 
+def ridgeRegres(xMat, yMat, lam = 0.2):
+    xTx = xMat.T * xMat
+    denom = xTx + eye(shape(xMat)[1]) * lam
+    if linalg.det(denom) == 0.0:
+        print "this matrix is singular, cannot do inverse"
+        return
+
+    ws = denom.I * (xMat.T * yMat)
+
+    return ws
+
+def ridgeTest(xArr, yArr):
+    xMat = mat(xArr)
+    yMat = mat(yArr).T
+    yMean = mean(yMat, 0)
+    yMat = yMat - yMean
+    xMeans = mean(xMat, 0)
+    xVar = var(xMat, 0)
+    xMat = (xMat - xMeans) / xVar
+    numTestPts = 30
+    wMat = zeros((numTestPts, shape(xMat)[1]))
+    for i in range(numTestPts):
+        ws = ridgeRegres(xMat, yMat, exp(i - 10))
+        wMat[i, :] = ws.T
+
+    return wMat
+
+def regularize(xMat):
+    inMat = xMat.copy()
+    inMeans = mean(inMat, 0)
+    inVar = var(inMat, 0)
+    inMat = (inMat - inMeans) / inVar
+
+    return inMat
+
+def stageWise(xArr, yArr, eps = 0.01, numIt = 100):
+    xMat = mat(xArr)
+    yMat = mat(yArr).T
+
+    yMean = mean(yMat, 0)
+    yMat = yMat - yMean
+    xMat = regularize(xMat)
+
+    m, n = shape(xMat)
+    returnMat = zeros((numIt, n))
+    ws = zeros((n, 1))
+    wsTest = ws.copy()
+    wsMax = ws.copy()
+
+    for i in range(numIt):
+        # print ws.T
+        lowestError = inf
+        for j in range(n): # greedy algorithm
+            for sign in [-1, 1]: # increase or decrease the weights
+                wsTest = ws.copy()
+                wsTest[j] += eps * sign # increase or decrease only one weight
+                yTest = xMat * wsTest # test the performance
+                rssE = rssError(yMat.A, yTest.A)
+                if rssE < lowestError: # if better
+                    lowestError = rssE
+                    wsMax = wsTest
+        ws = wsMax.copy()
+        returnMat[i, :] = ws.T
+
+    return returnMat
